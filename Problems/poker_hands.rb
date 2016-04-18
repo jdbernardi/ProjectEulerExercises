eb64cdc22b0419ed1058f553b94ffb64ddaaa25d
Joe Bernardi <joebernardi@hotmail.com>
2016-04-18 14:21:19 -0500

    add pair, high value and 3 of a kind

--- Problems/poker_hands.rb
+++ Problems/poker_hands.rb
@@ -23,39 +23,117 @@ How many hands does Player 1 win?
 =end
 
 # PROGRAM poker_hands(player_one_hand, player_2_hand)
-def poker_hands(player_one_hand, player_2_hand)
+def poker_hands(player_1_hand, player_2_hand)
 # each suit and number is ranked
 # 2HCDS - A HCDS
-player_1 = player_one_hand.split(' ')
-player_2 = player_one_hand.split(' ')
-
-print player_1
-print player_2
-
-cards = { 
-  :H2 => 'H2',
-  :D2 => 'D2',
-  :C2 => 'C2',
-  :S2 => 'S2',
-  :H3 => 'H3',
-  :D3 => 'D3',
-  :C3 => 'C3',
-  :S3 => 'S3',
-  :H4 => 'H4',
-  :D4 => 'D4',
-  :C4 => 'C4',
-  :S4 => 'S4',
-
-   }
-
 
 # the hand of each player needs to be placed into an array
+player_1 = player_1_hand.split(" ")
+player_2 = player_2_hand.split(" ")
+
+print player_1
+puts ""
+puts ""
+print player_2
+puts ""
+
+cards_suits = [
+  "2H", "2D", "2C", "2S",
+  "3H", "3D", "3C", "3S",
+  "4H", "4D", "4C", "4S",
+  "5H", "5D", "5C", "5S",
+  "6H", "6D", "6C", "6S",
+  "7H", "7D", "7C", "7S",
+  "8H", "8D", "8C", "8S",
+  "9H", "9D", "9C", "9S",
+  "TH", "TD", "TC", "TS",
+  "JH", "JD", "JC", "JS",
+  "QH", "QD", "QC", "QS",
+  "KH", "KD", "KC", "KS",
+  "AH", "AD", "AC", "AS"
+]
+
+cards_rank = {
+  "2H" => 2, "2D" => 2, "2C" => 2, "2S" => 2,
+  "3H" => 3, "3D" => 3, "3C" => 3, "3S" => 3,
+  "4H" => 4, "4D" => 4, "4C" => 4, "4S" => 4,
+  "5H" => 5, "5D" => 5, "5C" => 5, "5S" => 5,
+  "6H" => 6, "6D" => 6, "6C" => 6, "6S" => 6,
+  "7H" => 7, "7D" => 7, "7C" => 7, "7S" => 7,
+  "8H" => 8, "8D" => 8, "8C" => 8, "8S" => 8,
+  "9H" => 9, "9D" => 9, "9C" => 9, "9S" => 9,
+  "TH" => 10,"TD" => 10,"TC" => 10,"TS" => 10,
+  "JH" => 11,"JD" => 11,"JC" => 11,"JS" => 11,
+  "QH" => 12,"QD" => 12,"QC" => 12,"QS" => 12,
+  "KH" => 13,"KD" => 13,"KC" => 13,"KS" => 13,
+  "AH" => 14,"AD" => 14,"AC" => 14,"AS" => 14
+}
+
+player1_rank = []
+player2_rank = []
+
+  puts ""
 # then each hand must be evaluted indepently
+
   # for each card the possible hands must be evaluated
   # High card
+  player_1.each { |x|
+  cards_rank.each { |k, v|
+    if x == k
+      player1_rank << v
+    end
+    }
+  }
+
+  player_2.each { |x|
+  cards_rank.each { |k, v|
+    if x == k
+      player2_rank << v
+    end
+    }
+  }
+
+  if player1_rank.max > player2_rank.max
+    puts "Player one has the high card of #{player1_rank.max}"
+  else
+    puts "Player two has the high card of #{player2_rank.max}"
+  end
+
   # One pair
+  pair_1 = player1_rank.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+  pair_2 = player2_rank.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+
+  pair_1.each { |k, v| 
+    if v == 2
+      print "player one has a pair of #{k}'s"
+    else
+    end
+  }
+
+  pair_2.each { |k, v| 
+    if v == 2
+      print "player two has a pair of #{k}'s"
+    else
+    end
+  }  
   # Two pairs
   # Three of a kind
+  three_1 = player1_rank.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+  three_2 = player2_rank.inject(Hash.new(0)) { |total, e| total[e] += 1; total}
+
+  three_1.each { |k, v| 
+    if v == 2
+      print "player one has three #{k}'s"
+    else
+    end
+  }
+
+  three_2.each { |k, v| 
+    if v == 2
+      print "player two has three #{k}'s"
+    else
+    end
+  }
   # Straight
   # Flush
   # Full House
@@ -68,4 +146,4 @@ cards = {
 
 end
 
-poker_hands('8C TS KC 9H 4S', '7D 2S 5D 3S AC')
+poker_hands("8C TS 8S 9H 8D", "7D 2S 5D 2D AC")
